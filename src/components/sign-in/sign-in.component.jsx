4b4ab0f1f7c5575bde3300c646a3b8593594aaee
Arnaldo Baumanis <arnabau@gmail.com>
2019-08-17 21:50:20 -0500

    Add Firebase and create sign in with Google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -7,8 +7,8 @@ import { signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
 
     this.state = {
       email: '',
@@ -27,17 +27,19 @@ class SignIn extends React.Component {
     this.setState({ [name]: value });
   }
 
-  render () {
+  render() {
     return (
-      <div className = 'sign-in'>
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email ann password</span>
 
-        <form onSubmit = {this.handleSubmit}>
-          <FormInput name = 'email' type = 'email' label = 'Email' value = {this.state.email} handleChange = {this.handleChange} required />
-          <FormInput name = 'password' type = 'password' label = 'password' value = {this.state.password} handleChange = {this.handleChange} required />
-          <CustomBotton type = 'submit'>Sign in</CustomBotton>
-          <CustomBotton onClick = {signInWithGoogle}>Sign in with Google</CustomBotton>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput name='email' type='email' label='Email' value={this.state.email} handleChange={this.handleChange} required />
+          <FormInput name='password' type='password' label='password' value={this.state.password} handleChange={this.handleChange} required />
+          <div className='buttons'>
+            <CustomBotton type='submit'>Sign in</CustomBotton>
+            <CustomBotton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomBotton>
+          </div>
         </form>
       </div>
     )
